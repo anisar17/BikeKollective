f140d99d4e48a1463f0b1a9ee0a77131e85c8704
Anisa <anisa.ramirez17@gmail.com>
2024-11-01 22:57:28 -0700

    Changed authentication_screen
    Update screen so now it connects to google sign in

--- bike_kollective/lib/authentication_screen.dart
+++ bike_kollective/lib/authentication_screen.dart
@@ -1,34 +1,97 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:bike_kollective/data/model/user.dart';
 import 'package:bike_kollective/data/provider/database.dart';
 import 'package:bike_kollective/data/provider/active_user.dart';
+import 'package:google_sign_in/google_sign_in.dart';
 
-class AuthenticationScreen extends ConsumerWidget {
+class AuthenticationScreen extends StatelessWidget {
+  const AuthenticationScreen({super.key});
+
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
-    // Watch the active user provider
-    final activeUser = ref.watch(activeUserProvider);
-
+  Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Authenticate"),
-      ),
-      body: Center(
-        child: ElevatedButton(
-          onPressed: () async {
-            // Get the notifier from the provider and call signIn
-            final notifier = ref.read(activeUserProvider.notifier);
-            await notifier.signIn();
-            
-            if (activeUser != null) {
-              // Proceed to the next screen or perform necessary actions
-              Navigator.pushReplacementNamed(context, '/home');
-            }
-          },
-          child: Text("Sign In with Google"),
-        ),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          Expanded(
+            flex: 3,
+            child: Padding(
+              padding: const EdgeInsets.all(10.0),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  const SizedBox(height: 20),
+                  const Text(
+                    'User Login',
+                    style: TextStyle(
+                      fontSize: 28,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white,
+                    ),
+                  ),
+                  const Spacer(flex: 2),
+                ],
+              ),
+            ),
+          ),
+          Expanded(
+            flex: 2,
+            child: Padding(
+              padding: const EdgeInsets.all(20.0),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  const SizedBox(height: 15),
+                  const Text(
+                    'The Bike Kollective user account is linked to your Google account to veryify and authenticate your information.',
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      fontSize: 16,
+                    ),
+                  ),
+                  const SizedBox(height: 30),
+                  ElevatedButton(
+                    onPressed: () {
+                      signInWithGoogle();
+                    },
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.blueAccent.shade700,
+                      padding: const EdgeInsets.symmetric(
+                        vertical: 15,
+                        horizontal: 80,
+                      ),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(30),
+                      ),
+                    ),
+                    child: const Text(
+                      'Login',
+                      style: TextStyle(
+                        fontSize: 18,
+                        color: Colors.white,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
       ),
     );
+  }
+
+  signInWithGoogle() async {
+    GoogleSignInAccount? googleUser = await GoogleSignIn().signIn();
+    GoogleSignInAuthentication? googleAuth = await googleUser?.authentication;
+
+    AuthCredential credentials = GoogleAuthProvider.credential(
+      accessToken: googleAuth?.accessToken,
+      idToken: googleAuth?.idToken
+    );
+    UserCredential userCredential = await FirebaseAuth.instance.signInWithCredential(credentials);
+    print(userCredential.user?.displayName);
   }
 }
